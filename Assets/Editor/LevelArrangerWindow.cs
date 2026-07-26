55626ee06a9f79faf319be0dc6a6bd5522ab1265
ChaitanyaJainYT <jainchaitanya30@gmail.com>
2026-07-26 18:45:34 +0530

    Update LevelArrangerWindow.cs

--- Assets/Editor/LevelArrangerWindow.cs
+++ Assets/Editor/LevelArrangerWindow.cs
@@ -256,6 +256,7 @@ public class LevelArrangerWindow : EditorWindow
                     {
                         SaveToBuildSettings();
                         LoadExistingPreviews();
+                        Repaint();
                     }
                 }
                 break;
